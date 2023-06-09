b6969d013f626bab72576ad3f8c470332223cd5b
PeterCastilloG <peter.castillo@globals.one>
2023-06-09 17:18:08 -0500

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { PopupModal } from 'react-calendly'
-import { BsFillCalendarPlusFill } from "react-icons/bs"
+import { InlineWidget } from 'react-calendly'
 import { IoMail } from "react-icons/io5"
 import { RiWhatsappFill } from "react-icons/ri"
 import { useState } from "react"
@@ -9,23 +8,18 @@ import "./App.scss"
 
 
 
+
 function App() {
   const [openCalendar, setCalendar] = useState(false)
 
+  const calendlyStyles = {
+    height: '500px', 
+    width: "500px",
+    overflow: 'hidden', 
+    padding: '1rem'
+  };
   return (
     <div className="container">
-      <PopupModal
-        open={openCalendar}
-        rootElement={document.getElementById("root")!}
-        onModalClose={() => setCalendar(false)}
-        pageSettings={{
-          backgroundColor: '1F1F29',
-          hideEventTypeDetails: true,
-          hideLandingPageDetails: true,
-          primaryColor: '9494A0',
-          textColor: 'E8E8E9',
-          hideGdprBanner: true,
-        }} url='https://calendly.com/globals1/entrevista' />
       <video id="background-video" autoPlay loop muted>
         <source src={video} type="video/mp4" />
         <source src={video} type="video/webm" />
@@ -40,21 +34,33 @@ function App() {
             <span>Entrevista Laboral</span>
             <span>Full Stack Developer</span>
           </div>
-        </div>
-        <div className='interview'>
-          <span className='calendar'><BsFillCalendarPlusFill onClick={()=> setCalendar(true)} /></span>
-          <div className='info'>
-            <span>Queremos Conocerte</span>
-            <p>Nos sentimos entusiasmados de tenerte en este
-              punto del proceso y estamos ansiosos por
-              conocer más sobre ti, tus experiencias, tus
-              habilidades y cómo podrías enriquecer nuestro
-              equipo, así explorar a profundidad tu potencial y
-              alinear tus principios y valores con los nuestros.
-              A través de este sistema, podrás seleccionar la
-              fecha y el horario que mejor se adapte a tu
-              disponibilidad.
-            </p>
+          <div className='interview'>
+            {/* <InlineWidget
+              pageSettings={{
+                backgroundColor: '1F1F29',
+                hideEventTypeDetails: true,
+                hideLandingPageDetails: true,
+                primaryColor: '9494A0',
+                textColor: 'E8E8E9',
+                hideGdprBanner: true,
+              }}
+              styles={calendlyStyles} // Aplica los estilos personalizados
+              url='https://calendly.com/globals1/entrevista'
+            /> */}
+            <div className='info'>
+              <span>Queremos Conocerte</span>
+              <p>Nos sentimos entusiasmados de tenerte en este
+                punto del proceso y estamos ansiosos por
+                conocer más sobre ti, tus experiencias, tus
+                habilidades y cómo podrías enriquecer nuestro
+                equipo, así explorar a profundidad tu potencial y
+                alinear tus principios y valores con los nuestros.
+                A través de este sistema, podrás seleccionar la
+                fecha y el horario que mejor se adapte a tu
+                disponibilidad.
+              </p>
+              <button>Agendar Entrevista</button>
+            </div>
           </div>
         </div>
         <div className='contact'>
